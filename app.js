b335c0b3c8705332c31f88ffa5679a7a08f6ce4f
Jamie Knight <jamie@ipcortex.co.uk>
2018-02-02 15:11:24 +0000

    Content length and cleanup
    Added content length to the downloads. As part of this we now store the
    ZIP file in a buffer; the upload is limited to 10MB in size so shouldn't
    cause a memory problem. Eventually it should probably stream that out to
    disk.
    
    Also uploads will now be cleaned up after they have been used.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const tnef = require('node-tnef');
 const express = require('express');
 const multer = require('multer');
 const jszip = require('jszip');
+const fs = require('fs');
 
 const app = express();
 const upload = multer({ dest: 'uploads/', limits: { fileSize: 10 * (1024 * 1024 * 8) } });
@@ -11,16 +12,29 @@ app.post('/upload', upload.single('file'), (req, res) => {
 	tnef.parse(req.file.path, (err, files) => {
 		if(err) {
 			res.send({ status: 'error', message: 'Parsing of TNEF file failed!' });
+			removeUpload(req.file.path);
 			return;
 		}
 		let zip = new jszip();
 		files.forEach((file) => zip.file(file.Title, new Buffer(file.Data)));
-		res.writeHead(200, {
-			'Content-disposition': 'attachment;filename=winmail.zip',
-			//'Content-Length': data.length
-		})
-		zip.generateNodeStream({type:'nodebuffer',streamFiles:true}).pipe(res);
+		zip.generateAsync({type: "nodebuffer"}).then((buffer) => {
+			res.writeHead(200, {
+				'Content-disposition': 'attachment;filename=winmail.zip',
+				'Content-Length': buffer.length
+			})
+			res.end(buffer);
+			removeUpload(req.file.path);
+		});
 	});
 });
+
+function removeUpload(file) {
+	fs.access(file, fs.constants.W_OK, (err) => {
+		if(err) {
+			return console.warn('Attempted to remove inaccessible file!', file);
+		}
+		fs.unlink(file, (err) => { if(err) console.warn('Unexpected error removing file:', err) });
+	});
+}
 
 app.listen(8080, () => console.log('App running on port 8080'));
